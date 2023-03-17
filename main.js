4c938290ef7586384b94d19bf42fd51765ad2dff
hydrashumor <110207922+hydrashumor@users.noreply.github.com>
2023-03-16 20:51:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+canvas = createCanvas(800,500);
+canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
